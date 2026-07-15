9a3f1ff89d78349a825e515db9f0f677ce488b40
petra309 <petrazurini@gmail.com>
2026-07-15 20:51:27 +0000

    muoio

--- esercizio1.py
+++ esercizio1.py
@@ -1,24 +1,46 @@
 #ESERCIZIO 1
+#introduzione
+
+'''
+Nome del programma: calcolatrice.py
+Autore: Mario Rossi
+Data: 23/07/2026
+Versione: 1.0
+Descrizione: Semplice script per eseguire le quattro operazioni base.
+'''
 
 #RISOLUZIONE PUNTO 1
 def is_pari(n) :
-    """ Ritorna vero se "n" è pari,se no ritorna falso """
+    """ Ritorna vero se "n" è pari, se no ritorna falso """
   
     risultato = False
 
-    if n%2 != 0 :
+    if n%2 == 0 :
         #% da resto divisione
         risultato = True
 
     return risultato
 ###
 
+#RISOLUZIONE PUNTO 2
+def generatore(n) :
+    """ Se il numero è intero e positivo applica la funzione is_pari"""
 
-main()
+    if type(n) == int and n > 0 :
+        is_pari(n) 
 
+    else :
+        print("numero non accettabile, riprovare")
+
+
+
+
+
+
+#main()
 numero = int( input('Dammi un numero: ') )
 print(type(numero))
-result = is_pari(numero)
+result = generatore(numero)
 
 print(result)
 
